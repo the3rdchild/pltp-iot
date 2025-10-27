fef409da63aa9ef521ad489b365ac8a30c0b4e97
the3rdchild <ytcrash555@gmail.com>
2025-10-27 04:42:18 +0000

    Update Nav-menu still bugging hiks

--- src/menu-items/dashboard.jsx
+++ src/menu-items/dashboard.jsx
@@ -1,16 +1,36 @@
 // assets
-import { DashboardOutlined } from '@ant-design/icons';
+import { 
+  DashboardOutlined,
+  BarChartOutlined,
+  ExperimentFilled,
+  HourglassOutlined,
+  DatabaseOutlined,
+  SettingOutlined,
+  EditOutlined,
+  SlidersOutlined,
+  FundViewOutlined,
+  LogoutOutlined
+} from '@ant-design/icons';
 
 // icons
 const icons = {
-  DashboardOutlined
+  DashboardOutlined,
+  BarChartOutlined,
+  ExperimentFilled,
+  HourglassOutlined,
+  DatabaseOutlined,
+  SettingOutlined,
+  EditOutlined,
+  SlidersOutlined,
+  FundViewOutlined,
+  LogoutOutlined
 };
 
 // ==============================|| MENU ITEMS - DASHBOARD ||============================== //
 
 const dashboard = {
   id: 'group-dashboard',
-  title: 'Navigation', // This title will appear above the dashboard item
+  title: 'Navigation',
   type: 'group',
   children: [
     {
@@ -20,6 +40,78 @@ const dashboard = {
       url: '/dashboard',
       icon: icons.DashboardOutlined,
       breadcrumbs: false
+    },
+    {
+      id: 'analytics',
+      title: 'Analytics',
+      type: 'collapse',
+      icon: icons.BarChartOutlined,
+      children: [
+        {
+          id: 'dryness',
+          title: 'Dryness',
+          type: 'item',
+          url: '/analytics/dryness',
+          icon: icons.ExperimentFilled,
+        },
+        {
+          id: 'ncg',
+          title: 'NCG',
+          type: 'item',
+          url: '/analytics/ncg',
+          icon: icons.HourglassOutlined,
+        },
+        {
+          id: 'tds',
+          title: 'TDS',
+          type: 'item',
+          url: '/analytics/tds',
+          icon: icons.DatabaseOutlined,
+        },
+        {
+          id: 'ptf',
+          title: 'P, T, F',
+          type: 'item',
+          url: '/analytics/ptf',
+          icon: icons.DashboardOutlined,
+        }
+      ]
+    },
+    {
+      id: 'settings',
+      title: 'Settings',
+      type: 'collapse',
+      icon: icons.SettingOutlined,
+      children: [
+        {
+          id: 'manual-data-input',
+          title: 'Manual Data Input',
+          type: 'item',
+          url: '/settings/manual-data-input',
+          icon: icons.EditOutlined,
+        },
+        {
+          id: 'calibration',
+          title: 'Calibration',
+          type: 'item',
+          url: '/settings/calibration',
+          icon: icons.SlidersOutlined,
+        },
+        {
+          id: 'limit',
+          title: 'Limit',
+          type: 'item',
+          url: '/settings/limit',
+          icon: icons.FundViewOutlined,
+        }
+      ]
+    },
+    {
+      id: 'logout',
+      title: 'Logout',
+      type: 'item',
+      url: '/logout',
+      icon: icons.LogoutOutlined
     }
   ]
 };
--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -1,13 +1,10 @@
 // project import
 import dashboard from './dashboard';
-import analytics from './analytics';
-import settings from './settings';
-import logout from './logout';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, analytics, settings, logout]
+  items: [dashboard]
 };
 
 export default menuItems;
